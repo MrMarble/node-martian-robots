348258cfa4d2ec5c444c7d21d77d734413364612
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2021-07-19 02:43:35 +0200

    test(cli): parseInputFile

---
+++ packages/cli/tests/parseInput.js
@@ -0,0 +1,23 @@
+const { test } = require("uvu");
+const assert = require("uvu/assert");
+const { parseInputFile } = require("../lib/parseInput");
+
+test(`parseInput`, async (t) => {
+  assert.equal(await parseInputFile("tests/fixtures/input.txt"), [
+    ["5", "3"],
+    [
+      ["1", "1", "E"],
+      ["R", "F", "R", "F", "R", "F", "R", "F"],
+    ],
+    [
+      ["3", "2", "N"],
+      ["F", "R", "R", "F", "L", "L", "F", "F", "R", "R", "F", "L", "L"],
+    ],
+    [
+      ["0", "3", "W"],
+      ["L", "L", "F", "F", "F", "L", "F", "L", "F", "L"],
+    ],
+  ]);
+});
+
+test.run();
