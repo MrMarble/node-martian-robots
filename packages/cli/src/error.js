cc748c5845c4ba64a34c1d3221ab6a292407a00b
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2021-07-19 18:18:57 +0200

    fix(test): preconstruct broke everything

--- packages/cli/src/error.js
+++ packages/cli/src/error.js
@@ -1,7 +1,7 @@
 import chalk from "chalk";
 import { exit } from "process";
 
-export const error = message => {
+export const error = (message) => {
   console.error(`${chalk.red("Error:")} ${message}`);
   exit(1);
 };
